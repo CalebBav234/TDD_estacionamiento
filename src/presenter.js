75f662f5b0d63deff01034853c0b628493072c24
Caleb <calebbaving12@gmail.com>
2025-09-07 08:55:11 -0400

    feat: se implemento el ingreso de entrada en la UI para que use el usuario

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,18 @@
-import sumar from "./sumador";
+import { registrarIngreso } from "./parqueo.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const inicioInput = document.getElementById("inicio");
+const btnIngreso = document.getElementById("btnIngreso");
+const resultadoDiv = document.getElementById("resultado");
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+let horaIngreso = null;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+
+btnIngreso.addEventListener("click", () => {
+  try {
+    horaIngreso = registrarIngreso(inicioInput.value);
+    resultadoDiv.innerText = `Ingreso registrado: ${horaIngreso}`;
+  } catch (err) {
+    resultadoDiv.innerText = err.message;
+  }
 });
