e3167bd88ffad5d34993a8f2ef369070de9b97e5
Caleb <calebbaving12@gmail.com>
2025-09-08 14:44:14 -0400

    feat: se implemento el calculo de monto final a pagar en la UI

--- src/presenter.js
+++ src/presenter.js
@@ -1,22 +1,21 @@
-import { registrarIngreso, registrarSalida, calcularTarifaBasica, calcularDesglosePorDias } from "./parqueo.js";
+import { registrarIngreso, registrarSalida, calcularTarifaBasica, calcularDesglosePorDias, calcularMontoTotalFinal } from "./parqueo.js";
 
 const inicioInput = document.getElementById("inicio");
 const btnIngreso = document.getElementById("btnIngreso");
 const finInput = document.getElementById("fin");
 const btnSalida = document.getElementById("btnSalida");
 const resultadoDiv = document.getElementById("resultado");
-const desgloseDiv = document.getElementById("desglose");
+const montoDiv = document.getElementById("montoTotal");
 
 let horaIngreso = null;
 
 btnIngreso.addEventListener("click", () => {
   try {
     horaIngreso = registrarIngreso(inicioInput.value);
-    resultadoDiv.innerText = `Ingreso registrado: ${new Date(horaIngreso).toLocaleString()}`;
-    desgloseDiv.innerHTML = "";
+    resultadoDiv.innerText = `Ingreso registrado: ${horaIngreso}`;
+    montoDiv.innerHTML = "";
   } catch (err) {
     resultadoDiv.innerText = err.message;
-    desgloseDiv.innerHTML = "";
   }
 });
 
@@ -27,22 +26,30 @@ btnSalida.addEventListener("click", () => {
       return;
     }
     const horaSalida = registrarSalida(finInput.value, horaIngreso);
-    resultadoDiv.innerText = `Salida registrada: ${new Date(horaSalida).toLocaleString()}`;
+    resultadoDiv.innerText = `Salida registrada: ${horaSalida}`;
     const desglose = calcularDesglosePorDias(horaIngreso, horaSalida);
-    let desgloseHtml = "<h3>Desglose por día:</h3><ul>";
+    const totalFinal = calcularMontoTotalFinal(horaIngreso, horaSalida);
+    let html = `<h3>Desglose por día</h3>
+      <table border="1" cellpadding="5">
+        <tr>
+          <th>Fecha</th>
+          <th>Monto sin tope</th>
+          <th>Monto con tope</th>
+        </tr>`;
     desglose.forEach(d => {
-      desgloseHtml += `<li>
-        <b>${d.fecha}</b>: 
-        Sin tope Bs ${d.montoSinTope.toFixed(2)} | 
-        Con tope Bs ${d.montoConTope.toFixed(2)}
-      </li>`;
+      html += `
+        <tr>
+          <td>${d.fecha}</td>
+          <td>Bs ${d.montoSinTope}</td>
+          <td>Bs ${d.montoConTope}</td>
+        </tr>`;
     });
-    desgloseHtml += "</ul>";
-    desgloseDiv.innerHTML = desgloseHtml;
-    const total = desglose.reduce((sum, d) => sum + d.montoConTope, 0);
+    html += `</table>`;
+    html += `<h3>Total final a pagar: Bs ${totalFinal}</h3>`;
+    montoDiv.innerHTML = html;
   } catch (err) {
     resultadoDiv.innerText = err.message;
-    desgloseDiv.innerHTML = "";
+    montoDiv.innerHTML = "";
   }
 });
 
